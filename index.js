5b7b60b86c4334c5f61b114d2d5776704a05d50f
johnie261 <njorogejohn261@gmail.com>
2023-02-21 01:07:35 +0300

    Add content in index.js file

---
+++ index.js
@@ -0,0 +1,6 @@
+import './modules/constructor.js';
+import './modules/links.js';
+import './modules/addRemove.js';
+import './modules/load.js';
+import './modules/addRemoveBtn.js';
+import './modules/datime.js';
